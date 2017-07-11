764c6f729a499585948fc0d9e03a3653294c6315
Himanshu Singh <himansingh2016@gmail.com>
2017-07-12 00:11:15 +0530

    modified code

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
-	$(".cssload-container").hide();
+	$(".cssload-container").hide(); // hiding loader initially
 
-	var myToken = "EAACEdEose0cBAG7lGydxvEHnZCqiqoUMLFDuonqtY8Cisp3MDSmEFO5pc01xas1oBbiLlNBoiLxeuWb9yfZAEHzUr3UpXVPHBtW5RZCk2a0yN9NWAAMZCnJwh9ofW7aUE7LNWtwzZAMw3siYp4EWLZAhZBYFS8h7daAWBbP78vKniGNcCAbST8epNlcNOnuZA6QZD";
+	var myToken = "EAACEdEose0cBAAuYIgHdc6JOIfYk87uCa6WnzwQaC3UJF7ICZAFA6ixO5hG1MZCGwrubY3lZA2Kx6maDZAGDikruD4OEaZBeLIqsCMZBuZCcFeOBQeF6vFvJzn08OuhjuIbc9LvfsDqwuDIZCuQjcobogK7BBxEt2Jf2MlYHFhWLTPyWGy8kZCc88n49BiYgWiLMZD";
 
 	$("#basicInfoTab").on('click' , function(){
 
@@ -17,40 +17,53 @@ $(document).ready(function(){
 
 					$('#profileData').css ("background-color" , "#e9ebee");
 
-					$("#basicInfo-data").show();
+					$("#basicInfo-data").show(); 
 
+				
 					$('#name1').text("Full Name");
 					$('#val1').text(response.name);
 					
-					$('#name2').text("D.O.B");
-					$('#val2').text(response.birthday);
+					if(response.birthday !== null && response.birthday !== undefined){
+						$('#name2').text("D.O.B");
+						$('#val2').text(response.birthday);
+					}
+
+					if(response.email !== null && response.email !== undefined){
+						$('#name3').text("Email");
+						$('#val3').text(response.email);
+					}
+
+					if(response.gender !== null && response.gender !== undefined){
+						$('#name4').text("Gender");
+						$('#val4').text(response.gender);
+					}	
 					
-					$('#name3').text("Email");
-					$('#val3').text(response.email);
-					
-					$('#name4').text("Gender");
-					$('#val4').text(response.gender);
-					
-					$('#name5').text("Location");
-					$('#val5').text(response.location.name);
-					
-					$('#name6').text("Relationship Status");
-					$('#val6').text(response.relationship_status);
-					
-					if(response.political !== null || response.political !== undefined){
+					if(response.location.name !== null && response.location.name !== undefined){
+						$('#name5').text("Location");
+						$('#val5').text(response.location.name);
+					}
+
+					if(response.relationship_status !== null && response.relationship_status !== undefined){
+						$('#name6').text("Relationship Status");
+						$('#val6').text(response.relationship_status);
+					}
+
+					if(response.political !== null && response.political !== undefined){
 						$('#name7').text("Political Views");
 						$('#val7').text(response.political);
 
 					}
 					
-					if(response.religion !== null || response.religion !== undefined){
+					if(response.religion !== null && response.religion !== undefined){
 						$('#name8').text("Religion");
 						$('#val8').text(response.religion);
 					}
 					
-					if(response.political !== null || response.political !== undefined){
+					if(response.languages !== null && response.languages !== undefined){
 						$('#name9').text("Languages");
-						$('#val9').text( response.languages[0].name + " , " + response.languages[1].name);
+
+						for(var index=0 ; index < (response.languages).length ; index++ )
+						$('#val9').text( response.languages[index].name );
 					}	
 					
 				},
@@ -103,7 +116,7 @@ $(document).ready(function(){
 					
 					for(var i in response.education){
 						
-						if(response.education[i].school.name !== null || response.education[i].school.name !== undefined)
+						if(response.education[i].school.name !== null && response.education[i].school.name !== undefined)
 						$('#val1' +i).text(response.education[i].school.name);
 
 						else {
@@ -158,46 +171,39 @@ $(document).ready(function(){
 
 				success : function (response) {
 
-					$('#profileData').css ("background-color" , "#e9ebee");
+					$('#profileData').css("background-color" , "#e9ebee");
 
 					$("#fav-data").show();
-					
-					$('#name11').text("TOP 5 FAVOURITE ATHELETES");
-					
-					for(var i=0 ;i<6 ; i++ ){
 
-						
-						if( response.favorite_athletes[i].name !== null || response.favorite_athletes[i].name !== undefined)
+					var atheletsPos =0;
+					
+					// Printing favourite atheletes
+					$('#name11').text("TOP FAVOURITE ATHELETES");
+					
+					for(var i in response.favorite_athletes ){
+
+						( response.favorite_athletes[i].name !== undefined && response.favorite_athletes[i].name !== null)
 						{
-							$('#val1' + (i+6)).text(response.favorite_athletes[i].name);
-							
+							$('#val1' + (atheletsPos+6)).text(response.favorite_athletes[i].name);
+							atheletsPos++;
 						}
 
-						else{
-
-							alert("Data not found!!");
-							break;
-						}
-
-					
+											
 					} // end of loop
 
+					// to print favourite teams
+					var teamPos = 0;
+
+					$('#name12').text("TOP  FAVOURITE TEAMS");
 					
+					for(var x in response.favorite_teams){
 
-					$('#name12').text("TOP 5 FAVOURITE TEAMS");
-					
-					for(var i=0 ;i<6 ; i++ ){
-
-						if( response.favorite_teams[i].name !== null || response.favorite_teams[i].name !== undefined )
-						$('#val2' + (i+1)).text(response.favorite_teams[i].name);
-
-						else{
-
-							alert("Data not found!!");
-							break;
-						}
-
-					}
+						if( response.favorite_teams[x].name !== null && response.favorite_teams[x].name !== undefined )
+						 {	$('#val2' + (teamPos+1)).text(response.favorite_teams[x].name);
+							teamPos++;
+							}
+						
+					}// end of loop
 					
 										
 				},
@@ -253,18 +259,18 @@ $(document).ready(function(){
 
 					var element=(response.posts.data);
 
-					if(element[i].story !== null || element[i].story !== undefined){
+					if(element[i].story !== null && element[i].story !== undefined){
 						$("#story"+ i).text(element[i].story);
 						$("#story"+i).css("font-weight" , "700");
 					}
 					
-					if(element[i].created_time !== null || element[i].created_time !== undefined)
+					if(element[i].created_time !== null && element[i].created_time !== undefined)
 						$("#time"+i).text(element[i].created_time);
 
-					if(element[i].message !== null || element[i].message !== undefined)
+					if(element[i].message !== null && element[i].message !== undefined)
 						$("#msg"+i).text(element[i].message);
 
-					if(element[i].description !== null || element[i].description !== undefined){
+					if(element[i].description !== null && element[i].description !== undefined){
 						$("#description"+i).text(element[i].description);
 						$("#description"+i).css("font-style" , "italic");
 
@@ -298,7 +304,13 @@ $(document).ready(function(){
 			}
 
 			}) ;// end of ajax call
+	
 	}); // end of feed tab function
 
 
 }); // end of document
+
+function favTeam(){
+
+
+}
